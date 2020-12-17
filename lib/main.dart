a72d8a070897c2c27c9134724c05757310e34671
swaran5 <73553856+swaran5@users.noreply.github.com>
2020-12-17 20:10:41 +0530

    Removed Unwanted Code and Formated lines

--- lib/main.dart
+++ lib/main.dart
@@ -37,154 +37,132 @@ class MyApp extends StatelessWidget {
 class ScrollingPart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    AssetImage assetImage1 = AssetImage('images/image1.png');
-    Widget image1 = FittedBox(child: Image(image: assetImage1), fit: BoxFit.fill);
-
-    AssetImage assetImage2 = AssetImage('images/image2.png');
-    Widget image2 =
-        FittedBox(child: Image(image: assetImage2), fit: BoxFit.fill);
-
-    AssetImage assetImage3 = AssetImage('images/image3.png');
-    Widget image3 =
-        FittedBox(child: Image(image: assetImage3), fit: BoxFit.fill);
-
-    AssetImage assetImage4 = AssetImage('images/image4.png');
-    Widget image4 =
-        FittedBox(child: Image(image: assetImage4), fit: BoxFit.fill);
-
-    AssetImage assetImage5 = AssetImage('images/image5.png');
-    Widget image5 =
-        FittedBox(child: Image(image: assetImage5), fit: BoxFit.fill);
-
-    // AssetImage assetImage6 = AssetImage('images/ayurveda.png');
-    // Widget image6 =
-    //     FittedBox(child: Image(image: assetImage6), fit: BoxFit.fill);
-    //
-    // AssetImage assetImage7 = AssetImage('images/family.png');
-    // Widget image7 =
-    //     FittedBox(child: Image(image: assetImage7), fit: BoxFit.fill);
-    //
-    // AssetImage assetImage8 = AssetImage('images/skincare.png');
-    // Widget image8 =
-    //     FittedBox(child: Image(image: assetImage8), fit: BoxFit.fill);
-
     return Container(
       child: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints viewportConstraints) {
           return SingleChildScrollView(
             child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                   Container(
-                    height: 200,
-                     margin: EdgeInsets.only(left: 0, top:0, right: 0, bottom:10),
-                    decoration:  BoxDecoration(
-                      image:  DecorationImage(
-                        image: AssetImage('images/image1.png'),
-                        fit: BoxFit.fill,
-                      ),
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  margin:
+                      EdgeInsets.only(left: 0, top: 0, right: 0, bottom: 10),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('images/image1.png'),
+                      fit: BoxFit.fill,
                     ),
                   ),
-                  Container(
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 150,
-                      width: 200,
-                          margin: EdgeInsets.only(left: 5, top:0, right: 5, bottom:0),
-                      decoration:  BoxDecoration(
-                        image:  DecorationImage(
-                          image: AssetImage('images/image2.png'),
-                          fit: BoxFit.fill,
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 150,
+                        width: 200,
+                        margin: EdgeInsets.only(
+                            left: 5, top: 0, right: 5, bottom: 0),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('images/image2.png'),
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
+                      Container(
+                        height: 150,
+                        width: 210,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('images/image3.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
                       ),
-                        Container(
-                          height: 150,
-                          width: 210,
-                          decoration:  BoxDecoration(
-                            image:  DecorationImage(
-                              image: AssetImage('images/image3.png'),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-
+                    ],
                   ),
-                  Container(
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 80,
-                          width: 200,
-                          margin: EdgeInsets.only(left: 5, top:0, right: 5, bottom:0),
-                          decoration:  BoxDecoration(
-                            image:  DecorationImage(
-                              image: AssetImage('images/image4.png'),
-                              fit: BoxFit.fill,
-                            ),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 80,
+                        width: 200,
+                        margin: EdgeInsets.only(
+                            left: 5, top: 0, right: 5, bottom: 0),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('images/image4.png'),
+                            fit: BoxFit.fill,
                           ),
                         ),
-                        Container(
-                          height: 80,
-                          width: 210,
-                          decoration:  BoxDecoration(
-                            image:  DecorationImage(
-                              image: AssetImage('images/image5.png'),
-                              fit: BoxFit.fill,
-                            ),
+                      ),
+                      Container(
+                        height: 80,
+                        width: 210,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('images/image5.png'),
+                            fit: BoxFit.fill,
                           ),
                         ),
-                      ],
-                    ),
-
+                      ),
+                    ],
                   ),
-                  Container(
+                ),
+                Container(
                     margin: EdgeInsets.all(10),
                     height: 140.0,
                     child: Column(
                       children: [
-                      Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("Shop By Category",style: TextStyle(fontWeight: FontWeight.w800),),
-                    ),
-
                         Align(
                           alignment: Alignment.centerLeft,
-                          child: Text("Extensive Range To Keep Your Family Healthy",style: TextStyle(fontWeight: FontWeight.w300),),
+                          child: Text(
+                            "Shop By Category",
+                            style: TextStyle(fontWeight: FontWeight.w800),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Extensive Range To Keep Your Family Healthy",
+                            style: TextStyle(fontWeight: FontWeight.w300),
+                          ),
                         ),
                         Row(
                           children: [
                             Container(
-                              margin: EdgeInsets.only(left: 30, top:20, right: 0, bottom:0),
+                              margin: EdgeInsets.only(
+                                  left: 30, top: 20, right: 0, bottom: 0),
                               height: 80,
                               width: 80,
-                              decoration:  BoxDecoration(
-                                image:  DecorationImage(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
                                   image: AssetImage('images/family.png'),
                                   fit: BoxFit.fill,
                                 ),
                               ),
                             ),
                             Container(
-                              margin: EdgeInsets.only(left: 60, top:20, right: 0, bottom:0),
+                              margin: EdgeInsets.only(
+                                  left: 60, top: 20, right: 0, bottom: 0),
                               height: 80,
                               width: 80,
-                              decoration:  BoxDecoration(
-                                image:  DecorationImage(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
                                   image: AssetImage('images/ayurveda.png'),
                                   fit: BoxFit.fill,
                                 ),
                               ),
                             ),
                             Container(
-                              margin: EdgeInsets.only(left: 60, top:20, right: 00, bottom:0),
+                              margin: EdgeInsets.only(
+                                  left: 60, top: 20, right: 00, bottom: 0),
                               height: 80,
                               width: 80,
-                              decoration:  BoxDecoration(
-                                image:  DecorationImage(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
                                   image: AssetImage('images/skincare.png'),
                                   fit: BoxFit.fill,
                                 ),
@@ -193,76 +171,84 @@ class ScrollingPart extends StatelessWidget {
                           ],
                         )
                       ],
-                    )
-                  ),
-                  Container(
-                    // Another fixed-height child.
-                    height: 30.0,
+                    )),
+                Container(
+                  // Another fixed-height child.
+                  height: 30.0,
+                  alignment: Alignment.centerLeft,
+                  child: Row(children: [
+                    Container(
+                        margin: EdgeInsets.only(
+                            left: 50, top: 0, right: 0, bottom: 0),
+                        child: Text(
+                          '60% Off',
+                          style: TextStyle(fontWeight: FontWeight.w800),
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            left: 75, top: 0, right: 00, bottom: 0),
+                        child: Text(
+                          '35% Off',
+                          style: TextStyle(fontWeight: FontWeight.w800),
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            left: 75, top: 0, right: 00, bottom: 0),
+                        child: Text(
+                          '55% Off',
+                          style: TextStyle(fontWeight: FontWeight.w800),
+                        )),
+                  ]),
+                ),
+                Container(
+                  // Another fixed-height child.
+                  height: 30.0,
+                  alignment: Alignment.centerLeft,
+                  child: Row(children: [
+                    Container(
+                        margin: EdgeInsets.only(
+                            left: 65, top: 0, right: 0, bottom: 0),
+                        child: Text(
+                          'PPE',
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            left: 85, top: 0, right: 00, bottom: 0),
+                        child: Text(
+                          'Ayurveda',
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            left: 75, top: 0, right: 00, bottom: 0),
+                        child: Text(
+                          'Skin Care',
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        )),
+                  ]),
+                ),
+                Container(
+                  height: 40.0,
+                  alignment: Alignment.center,
+                  child: Align(
                     alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Container(
-                            margin: EdgeInsets.only(left: 50, top:0, right: 0, bottom:0),
-
-                            child: Text('60% Off', style: TextStyle(fontWeight: FontWeight.w800),)
-                        ),
-                        Container(
-                            margin: EdgeInsets.only(left: 75, top:0, right: 00, bottom:0),
-
-                            child: Text('35% Off', style: TextStyle(fontWeight: FontWeight.w800),)
-                        ),
-                        Container(
-                            margin: EdgeInsets.only(left: 75, top:0, right: 00, bottom:0),
-
-                            child: Text('55% Off', style: TextStyle(fontWeight: FontWeight.w800),)
-                        ),
-                      ]
-                    ),
-                  ),
-                  Container(
-                    // Another fixed-height child.
-                    height: 30.0,
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                        children: [
-                          Container(
-                              margin: EdgeInsets.only(left: 65, top:0, right: 0, bottom:0),
-
-                              child: Text('PPE', style: TextStyle(fontWeight: FontWeight.w600),)
-                          ),
-                          Container(
-                              margin: EdgeInsets.only(left: 85, top:0, right: 00, bottom:0),
-
-                              child: Text('Ayurveda', style: TextStyle(fontWeight: FontWeight.w600),)
-                          ),
-                          Container(
-                              margin: EdgeInsets.only(left: 75, top:0, right: 00, bottom:0),
-
-                              child: Text('Skin Care', style: TextStyle(fontWeight: FontWeight.w600),)
-                          ),
-                        ]
-                    ),
-                  ),
-                  Container(
-                    height: 40.0,
-                    alignment: Alignment.center,
-                    child:  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Center(
-                          child: ElevatedButton(
-                            child: Text('Show Cart'),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => DetailsScreen()),
-                              );
-                            },
-                          ),
+                    child: Center(
+                      child: ElevatedButton(
+                        child: Text('Show Cart'),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DetailsScreen()),
+                          );
+                        },
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
           );
         },
       ),
@@ -282,9 +268,9 @@ class MyStatefulWidget extends StatefulWidget {
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static List<Widget> _widgetOptions = <Widget>[
-  HomeWidget(),
+    HomeWidget(),
     Text(
       'Offer Section',
       style: optionStyle,
@@ -352,7 +338,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 }
 
 class HomeWidget extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
